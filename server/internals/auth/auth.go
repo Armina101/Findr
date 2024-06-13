49910c00d9f1745418ed7861b9bdd95421680d31
TheBraveByte <ayaaakinleye@gmail.com>
2024-06-13 22:56:41 +0100

    Included authentication logic

---
+++ server/internals/auth/auth.go
@@ -0,0 +1,61 @@
+package auth
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	// "github.com/nhia/nhia-c/pkg/log"
+)
+
+// var lg = log.NewLogger()
+
+type Token struct {
+	jwt.RegisteredClaims
+	AccessCode string
+	UserId     string
+}
+
+// CreateJWT this function help to create a new auth token to help authenticate user
+func Create(id string, accessCode string) (string, error) {
+
+	// setting the timeout claims for the token to create
+	claims := Token{
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "NHIA_DEV",
+			Subject:   "Access Authentication",
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+		AccessCode: accessCode,
+		UserId:     id,
+	}
+
+	// create a new uer token for authentication
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(os.Getenv("SECRET_KEY")))
+	if err != nil {
+		// lg.Error.Println(err.Error())
+		return "", err
+	}
+
+	// lg.Info.Println("JWT token created")
+
+	return token, nil
+}
+
+func Parse(authToken string) (*Token, error) {
+	token, err := jwt.ParseWithClaims(authToken, &Token{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+	if err != nil {
+		// lg.Error.Println("cannot parse auth token : ", err.Error())
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(*Token); ok && token.Valid {
+		// lg.Info.Println("token parsed is valid ")
+		return claims, nil
+	}
+
+	return nil, err
+}
