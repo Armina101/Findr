ab82e2b99b692f18abce8aa4a45e8c2f96787c5c
TheBraveByte <ayaaakinleye@gmail.com>
2024-06-13 23:07:47 +0100

    Created server setup with httpserver

---
+++ server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/thebravebyte/linker-app/db"
+	"github.com/thebravebyte/linker-app/internals/controller"
+	"github.com/thebravebyte/linker-app/httpserver"
+)
+
+func main() {
+	// Setting up the logger
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	logger.Info("Starting the LinkerApp service")
+
+	URI := os.Getenv("DB_URI")
+
+	if URI == "" {
+		logger.Error("DB_URI is not set or unavailable")
+	}
+
+	logger.Info("Starting the LinkerApp service")
+
+	// OpenConnection: function to open a connection to the database
+	DBclient := db.OpenConnection(URI, logger)
+
+	
+	// Setting up the server configuration with actually values or details
+	srvCfg := &httpserver.SrvConfig{
+		Address:      ":8080",
+		ReadTimeout:  15,
+		WriteTimeout: 15,
+		IdleTimeout:  60,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	mainServer := httpserver.ConfigServer(srvCfg, logger)
+	mainServer.Use(mainServer.AddMiddleware)
+	mainServer.Use(mainServer.LoggingAppRequest(logger))
+
+	// import the funtion fro the controller that returns the service interface
+	lk := controller.NewFindr(DBclient, logger)
+
+	// Call the handlers from routes which havr the services interface passed as parameters
+	mainServer.AddRoutes(lk)
+
+
+	//  Todo: Ask fpr the path for these path of the statics files
+	mainServer.StaticFile("/favicon.ico", "./static/favicon.ico")
+	mainServer.StaticFile("/static", "./static")
+	// Todo: Reminder
+
+
+	// Starting the application
+	mainServer.StartApp()
+
+}
