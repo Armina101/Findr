7c43dcab1ab8587cf05c56b418c6de428d8dd67a
Aminat Afolabi <Afolabijolayemi@gmail.com>
2024-06-14 05:31:46 -0700

    Configuring new development environment

---
+++ findr/app/page.tsx
@@ -0,0 +1,20 @@
+import About from "@/components/shared/About";
+import Contact from "@/components/shared/Contact";
+import Hero from "@/components/shared/Hero";
+import Jobs from "@/components/shared/Jobs";
+import Sponsors from "@/components/shared/Sponsors";
+
+const Home = async () => {
+  const jobs = await prisma.jobPosting.findMany({});
+  return (
+    <>
+      <Hero jobs={jobs} />
+      <Jobs />
+      <Sponsors />
+      <About />
+      <Contact />
+    </>
+  );
+};
+
+export default Home;
