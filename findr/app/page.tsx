93823ecd5be4f91f15519858157a4a5b477d1653
Aminat Afolabi <Afolabijolayemi@gmail.com>
2024-06-15 07:51:26 -0700

    Making updates to Page 1

--- findr/app/page.tsx
+++ findr/app/page.tsx
@@ -3,6 +3,7 @@ import Contact from "@/components/shared/Contact";
 import Hero from "@/components/shared/Hero";
 import Jobs from "@/components/shared/Jobs";
 import Sponsors from "@/components/shared/Sponsors";
+import { prisma } from "@/lib/prisma";
 
 const Home = async () => {
   const jobs = await prisma.jobPosting.findMany({});
